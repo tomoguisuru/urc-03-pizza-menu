43dcaa0911254bf18d61376c093ebe1e8c287666
Brandon Drake <bdrake@edg.io>
2025-08-14 21:06:32 -0400

    Adds gh pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(), 
+    reactRouter(), 
+    tsconfigPaths(),
+  ],
+  base: 'urc-03-pizza-menu',
 });
